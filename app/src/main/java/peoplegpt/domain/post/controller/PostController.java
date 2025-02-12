58ee8ef65155f76ed9b59806c8222d62a791eca5
chans <herryjin@naver.com>
2025-02-11 23:56:45 -0800

    fix: return object type

--- app/src/main/java/peoplegpt/domain/post/controller/PostController.java
+++ app/src/main/java/peoplegpt/domain/post/controller/PostController.java
@@ -1,10 +1,7 @@
 package peoplegpt.domain.post.controller;
 
 import peoplegpt.domain.post.model.dto.response.PostListResponse;
-import peoplegpt.domain.post.model.entity.Post;
 import peoplegpt.domain.post.service.PostService;
-import peoplegpt.domain.post.service.impl.PostServiceImpl;
-import peoplegpt.domain.user.service.UserService;
 
 public class PostController {
 
@@ -14,12 +11,14 @@ public class PostController {
         this.postService = postService;
     }
 
-    public void displayPostsByCategory(String category) {
+    public PostListResponse displayPostsByCategory(String category) {
         PostListResponse response = postService.getPostsByCategory(category);
         if (response.getPosts().isEmpty()) {
             System.out.println("해당 카테고리의 게시물이 없습니다.");
         } else {
             System.out.println(response);
         }
+
+        return response;
     }
 }
